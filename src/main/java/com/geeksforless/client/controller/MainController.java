f01e688ebe5ca29d515c8006aa53dfa07d58a1f0
NikitaHurmaza <monkeykillerip@gmail.com>
2023-10-11 21:26:25 +0300

    temporary controller to easily start project

---
+++ src/main/java/com/geeksforless/client/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.geeksforless.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+public String mainPage(){
+        return "page";
+    }
+}
