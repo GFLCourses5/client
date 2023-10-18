a75e84ce2f7a7eb94e0e97a7fec34db45cd7da12
ykotsiuba <jurok3x@gmail.com>
2023-10-18 14:49:37 +0300

    Cleaned code.

--- src/main/java/com/geeksforless/client/model/entity/CustomUserDetails.java
+++ src/main/java/com/geeksforless/client/model/entity/CustomUserDetails.java
@@ -1,10 +1,12 @@
 package com.geeksforless.client.model.entity;
 
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+@Data
 public class CustomUserDetails implements UserDetails {
     private Long id;
     private String email;
