f2ccffd8e88a8e29c2893ad607d497945747001e
ykotsiuba <jurok3x@gmail.com>
2023-10-18 14:48:15 +0300

    Changed users migration

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users(password, email) VALUES('test1', 'test1@gmail.com');
-INSERT INTO users(password, email) VALUES('test2', 'test2@gmail.com');
-INSERT INTO users(password, email) VALUES('test3', 'test3@gmail.com');
+INSERT INTO users(password, email, name) VALUES('$2a$08$95ODixNWRwnYKHOfzB8VhOtIys4qw3.pKqd0tIYa2VMc2VNHiGQfC', 'test1@gmail.com', 'Foo');
+INSERT INTO users(password, email, name) VALUES('$2a$08$46lK6mkg6lXTiNXUL2.KleA5cKSWOf57XtZ4gPu5ijSgFI36V6Zra
+', 'test2@gmail.com', 'Bar');
+INSERT INTO users(password, email, name) VALUES('$2a$08$/QNeWViyP9jlIt/ml3l7AOtuevjaoZO4/e9tOi3y9qIP3MUtnxEFm', 'test3@gmail.com', 'John');
