67b53a5f4ec0df88278744d59441d379634bc5e9
ykotsiuba <jurok3x@gmail.com>
2023-10-19 23:33:31 +0300

    Fixed dto classes.

--- src/main/java/com/geeksforless/client/model/dto/scenario/ScenarioFormDTO.java
+++ src/main/java/com/geeksforless/client/model/dto/scenario/ScenarioFormDTO.java
@@ -2,9 +2,11 @@ package com.geeksforless.client.model.dto.scenario;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ScenarioFormDTO {
 
     private String scenariosList;
