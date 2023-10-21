b362fefd9be5411735d952f78aee1ca348e61e59
NikitaHurmaza <monkeykillerip@gmail.com>
2023-10-21 22:40:24 +0300

    added controller for step result page

--- src/main/java/com/geeksforless/client/controller/ResultController.java
+++ src/main/java/com/geeksforless/client/controller/ResultController.java
@@ -2,11 +2,13 @@ package com.geeksforless.client.controller;
 
 
 import com.geeksforless.client.model.dto.scenario.ScenarioResultDTO;
+import com.geeksforless.client.model.dto.scenario.StepResultDTO;
 import com.geeksforless.client.service.scenario.ScenarioService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,5 +30,11 @@ public class ResultController {
         return "result-page";
     }
 
-
+    @GetMapping("/scenarios/{id}")
+    public String showScenarioDetails(@PathVariable("id") Integer scenarioId, Model model) {
+        ScenarioResultDTO scenarioResultDTO = scenarioService.getScenarioById(scenarioId);
+        List<StepResultDTO> stepsResults = scenarioResultDTO.getStepsResults();
+        model.addAttribute("stepsResults", stepsResults);
+        return "stepResult-page";
+    }
 }
