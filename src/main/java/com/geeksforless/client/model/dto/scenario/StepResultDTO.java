db3510d9601e54b16c3fda71062e29d5fce051d0
Dima (IfLunatic) <dimasilenkoluna@gmail.com>
2023-10-21 15:28:03 +0300

    change annotation in ScenarioResultDTO.java, StepResultDTO.java

--- src/main/java/com/geeksforless/client/model/dto/scenario/StepResultDTO.java
+++ src/main/java/com/geeksforless/client/model/dto/scenario/StepResultDTO.java
@@ -1,8 +1,10 @@
 package com.geeksforless.client.model.dto.scenario;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class StepResultDTO {
     private Step step;
     private boolean isPassed;
