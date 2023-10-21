c6b9e5576c8c22878be29166be4ceaddb5bc3dec
Dima (IfLunatic) <dimasilenkoluna@gmail.com>
2023-10-21 15:29:32 +0300

    added result page and controller

---
+++ src/main/java/com/geeksforless/client/controller/ResultController.java
@@ -0,0 +1,30 @@
+package com.geeksforless.client.controller;
+
+
+import com.geeksforless.client.model.dto.scenario.ScenarioResultDTO;
+import com.geeksforless.client.service.scenario.ScenarioService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class ResultController {
+
+    private ScenarioService scenarioService;
+
+    @GetMapping("/scenarios")
+    public String showResults(Model model) {
+//        TODO: Fix getScenarioResults
+        List<ScenarioResultDTO> scenarioResultDTO = Arrays.asList(
+                scenarioService.getScenarioById(1),
+                scenarioService.getScenarioById(1),
+                scenarioService.getScenarioById(1));
+        model.addAttribute("scenarioResults", scenarioResultDTO);
+        return "result-page";
+    }
+}
