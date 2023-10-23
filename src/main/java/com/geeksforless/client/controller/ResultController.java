309b2245b010a8886440412673ce4f5797de542b
ykotsiuba <jurok3x@gmail.com>
2023-10-23 16:28:48 +0300

    Introduced Authentication parameter to fetch proper users id.

--- src/main/java/com/geeksforless/client/controller/ResultController.java
+++ src/main/java/com/geeksforless/client/controller/ResultController.java
@@ -5,11 +5,13 @@ import com.geeksforless.client.model.dto.scenario.ScenarioResultDTO;
 import com.geeksforless.client.model.dto.scenario.StepResultDTO;
 import com.geeksforless.client.service.scenario.ScenarioService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,12 +22,8 @@ public class ResultController {
     private ScenarioService scenarioService;
 
     @GetMapping("/scenarios")
-    public String showResults(Model model) {
-//        TODO: Fix getScenarioResults
-        List<ScenarioResultDTO> scenarioResultDTO = Arrays.asList(
-                scenarioService.getScenarioById(1),
-                scenarioService.getScenarioById(1),
-                scenarioService.getScenarioById(1));
+    public String showResults(Model model, Authentication authentication) {
+        List<ScenarioResultDTO> scenarioResultDTO = scenarioService.getScenarioResults(authentication);
         model.addAttribute("scenarioResults", scenarioResultDTO);
         return "result-page";
     }
