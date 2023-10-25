acd248f19a90214d3bb5b50614f371d533757698
Dima (IfLunatic) <dimasilenkoluna@gmail.com>
2023-10-25 17:24:53 +0300

    added annotation for ScenarioResultDTO.java

--- src/main/java/com/geeksforless/client/model/dto/scenario/ScenarioResultDTO.java
+++ src/main/java/com/geeksforless/client/model/dto/scenario/ScenarioResultDTO.java
@@ -2,12 +2,14 @@ package com.geeksforless.client.model.dto.scenario;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ScenarioResultDTO {
     private Integer id;
     private String name;
